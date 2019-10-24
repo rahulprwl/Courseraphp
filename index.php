2424b75a91c69e0eb9888f97229037f1b7ccbfac
rahulprwl <rahulrashmi.porwal@gmail.com>
2019-10-24 13:31:08 +0530

    initial code added

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>Guessing Game for Rahul Porwal 5a6ce3e5</title>
+<meta charset="UTF-8">
+</head>
+<body>
+<h1>Welcome to my guessing game</h1>
+<p>
+<?php
+    $corrnum=78;//$_REQUEST["Correct_number"];
+  if ( ! isset($_GET['guess']) ) { 
+    echo("Missing guess parameter");
+  } else if ( strlen($_GET['guess']) < 1 ) {
+    echo("Your guess is too short");
+  } else if ( !is_numeric($_GET['guess']) ) {
+    echo("Your guess is not a number");
+  } else if ( $_GET['guess'] < $corrnum ) {
+    echo("Your guess is too low");
+  } else if ( $_GET['guess'] > $corrnum ) {
+    echo("Your guess is too high");
+  } else {
+    echo("Congratulations - You are right");
+  }
+?>
+</p>
+</body>
+</html>
